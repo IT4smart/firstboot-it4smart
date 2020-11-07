cc45a96e4a4a1e5927fdac9d3aaf09755ad8558d
hack3d <hack3d.dev@googlemail.com>
2020-11-07 21:41:53 +0100

    Add dependencies to install the package. Restructure the package. Enable the user-notice-it4smart systemd service.

--- src/firstboot.sh
+++ src/firstboot.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -e
 logger -t "rc.firstboot" "Starting first boot actions"
 
+logger -t "rc.firstboot" "Enable user-notice-it4smart service"
+systemctl enable user-notice-it4smart.service
+
 logger -t "rc.firstboot" "Start provisioning"
 eth_device="$(ip route show default | awk '/default/ {print $5}')"
 mac_address="$(cat /sys/class/net/${eth_device}/address)"
